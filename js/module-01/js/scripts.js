89949edf80dd0f1192cdabd5a0b06b49e0a23f15
Andrii Riabozei <riabozei19@gmail.com>
2018-06-13 21:54:30 +0300

    Update scripts.js

--- js/module-01/js/scripts.js
+++ js/module-01/js/scripts.js
@@ -1,11 +1,11 @@
 'use strict';
 const ADMIN_LOGIN = 'admin';
 const ADMIN_PASSWORD = 'm4ngo1zh4ackz0r';
-const login = prompt('Введите, пожалуйста, ваш логин - ');
+const login = prompt('Введите, пожалуйста, ваш логин.');
 if (!login) {alert("Отменено пользователем!")}
 else if (login!==ADMIN_LOGIN) {alert('Доступ запрещен!')}
 else {
-    const password = prompt('Введите, пожалуйста, пароль - ');
+    const password = prompt('Введите, пожалуйста, пароль.');
     if (!password) {alert('Отменено пользователем!')}    
     else if (password!==ADMIN_PASSWORD) {alert('Доступ запрещен!')}
     else {alert('Добро пожаловать!')}
